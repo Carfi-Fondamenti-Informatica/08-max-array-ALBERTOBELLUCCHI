6248687ed99c07932a018090e6d45f98fc7cf6de
Alberto_Bellucchi <115469272+ALBERTOBELLUCCHI@users.noreply.github.com>
2022-12-09 17:52:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+float max(const float arr[], int dim) ;
+
+int main() {
+    int dim;
+    cin>>  dim;
+    float arr[dim];
+    for(int i=0; i<dim; i++) cin>>arr[i];
+    cout<<max(arr,dim);
+
+    return 0;
 }
