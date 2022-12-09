b81d6f8002d4e1e176c711b660569326dee6fdee
Alberto_Bellucchi <115469272+ALBERTOBELLUCCHI@users.noreply.github.com>
2022-12-09 17:52:56 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
 
+//
+// Created by alber on 09/12/2022.
+//
+
+#include "lib.h"
+float max(const float arr[], int dim) {
+    int pos_max = 0;
+    float max = arr[0];
+    for (int j = 0; j < dim; j++) {
+        if (arr[j] > max) {
+            max = arr[j];
+            pos_max = j;
+        }
+    }
+    return arr[pos_max];
+}
